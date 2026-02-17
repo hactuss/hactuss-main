1a53a54ebb7e21821b0afed8927476d6d00a5230
Hactuss <157395485+hactuss@users.noreply.github.com>
2026-02-17 19:54:31 +0100

    Add files via upload

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import "../app.css";
+
+    let { children } = $props();
+</script>
+
+<svelte:head>
+    <link
+        rel="stylesheet"
+        href="https://raw.githubusercontent.com/sindresorhus/modern-normalize/refs/heads/main/modern-normalize.css"
+    />
+</svelte:head>
+
+<nav style:border="solid red 1px">
+    <h1>layout.svelte</h1>
+</nav>
+{@render children()}
