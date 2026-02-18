129d06027d3794813b9d30d81be50a2285c1f6c5
hactuss <hactuss@proton.me>
2026-02-18 14:42:51 +0100

    new file:   .gitignore 	modified:   src/app.css 	modified:   src/routes/+page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,47 @@
 <script>
+    let content = $state("default");
+    let story = $state("");
+
+    function setDefault() {
+        content = "default";
+    }
 </script>
 
 <main>
+    <div>
+        <p>Hello, my [psydonym] is Hactuss.</p>
+    </div>
+    <div class="flex">
+        <div class="hea">creative</div>
+        <div class="hea">productive</div>
+        <div class="hea">free time</div>
+        <div class="hea">state</div>
+    </div>
     <div class="grid">
-        <div class="col cr" data-index="1">
+        <div
+            class="col cr"
+            on:mouseenter={() => {
+                content = "creative";
+            }}
+            on:mouseleave={() => {
+                setDefault();
+            }}
+        >
             <h4>creative</h4>
             <ul>
                 <li>art (ng)</li>
                 <li>video editing (yt)</li>
             </ul>
         </div>
-        <div class="col pr" data-index="2">
+        <div
+            class="col pr"
+            on:mouseenter={() => {
+                content = "productive";
+            }}
+            on:mouseleave={() => {
+                setDefault();
+            }}
+        >
             <h4>productive</h4>
             <ul>
                 <li>personal organization (obsidian)</li>
@@ -18,7 +49,15 @@
                 <li>ability farming</li>
             </ul>
         </div>
-        <div class="col ft" data-index="3">
+        <div
+            class="col ft"
+            on:mouseenter={() => {
+                content = "free time";
+            }}
+            on:mouseleave={() => {
+                setDefault();
+            }}
+        >
             <h4>free time</h4>
             <ul>
                 <li>linux rice</li>
@@ -26,7 +65,15 @@
                 <li>boredom</li>
             </ul>
         </div>
-        <div class="col st" data-index="4">
+        <div
+            class="col st"
+            on:mouseenter={() => {
+                content = "state";
+            }}
+            on:mouseleave={() => {
+                setDefault();
+            }}
+        >
             <h4>state</h4>
             <ul>
                 <li>future</li>
@@ -34,23 +81,59 @@
             </ul>
         </div>
     </div>
-    <div class="topic-content"><p>story</p></div>
+    <div class="topic-content">
+        <h2>{content}</h2>
+        <p>
+            {story}
+        </p>
+    </div>
 </main>
 
 <style>
+    :root {
+        --cr-color: rgba(255, 0, 0, 1);
+        --pr-color: rgba(255, 0, 0, 1);
+        --ft-color: rgba(255, 0, 0, 1);
+        --st-color: rgba(255, 0, 0, 1);
+    }
+    * {
+        accent-color: purple;
+    }
+
     .grid {
         display: grid;
         grid-template-columns: repeat(4, 1fr);
         grid-auto-rows: repeat(4, 1fr);
+
+        grid-template-areas: "";
         column-gap: 2%;
         margin: 1%;
     }
+    /*
+    .grid {
+        column-count: 4;
+    }*/
 
     .col {
-        border: solid white 1px;
-        border-image: linear-gradient(rgba(255, 0, 0, 1), rgba(0, 0, 0, 0)) 1;
-        border-radius: 100%;
-
-        min-height: 100px;
+        border: solid;
+        min-height: 200px;
+        width: 100%;
+    }
+    .cr {
+        border-image: linear-gradient(var(--cr-color), rgba(0, 0, 0, 0)) 1;
+    }
+    .pr {
+        border-image: linear-gradient(var(--pr-color), rgba(0, 0, 0, 0)) 1;
+    }
+    .ft {
+        border-image: linear-gradient(var(--ft-color), rgba(0, 0, 0, 0)) 1;
+    }
+    .st {
+        border-image: linear-gradient(var(--st-color), rgba(0, 0, 0, 0)) 1;
+    }
+    .flex {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
     }
 </style>
