0fdb1d910d21971551bc1bde361453f1673fa8e5
hactuss <hactuss@proton.me>
2026-02-18 16:25:53 +0100

    modified:   package.json 	modified:   pnpm-lock.yaml 	modified:   src/routes/+page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,8 @@
     function setDefault() {
         content = "default";
     }
+    let scrollText1 = $state("71");
+    let scrollText2 = $state("72");
 </script>
 
 <main>
@@ -87,14 +89,128 @@
             {story}
         </p>
     </div>
+
+    <div>
+        <h1>moing text</h1>
+        <div style:overflow="hidden">
+            <p class="marquee">
+                <span>1 2 3 4 5 6 7 8 9 0</span>
+            </p>
+            <p class="marquee2">
+                <span>&#160 1 2 3 4 5 6 7 8 9</span>
+            </p>
+        </div>
+    </div>
+    <div class="stc">
+        <p class="st one">{scrollText1}</p>
+        <p class="st two">{scrollText2}</p>
+    </div>
 </main>
 
-<style>
+<style lang="scss">
+    .marquee {
+        margin: 0 auto;
+        white-space: nowrap;
+        overflow: hidden;
+
+        border: solid red 1px;
+        width: 100%;
+    }
+
+    .marquee span {
+        display: inline-flex;
+        padding-left: 100%;
+        animation: marquee 10s linear infinite;
+    }
+    .marquee2 {
+        margin: 0 auto;
+        white-space: nowrap;
+        overflow: hidden;
+        border: solid red 1px;
+        width: 100%;
+    }
+    .marquee2 span {
+        display: inline-flex;
+        padding-left: 100%;
+        animation: marquee 10s linear infinite;
+    }
+    @keyframes marquee {
+        0% {
+            transform: translate(0, 0);
+        }
+        100% {
+            transform: translate(-100%, 0);
+        }
+    }
+    @keyframes marquee2 {
+        0% {
+            transform: translate(0, 0);
+        }
+        100% {
+            transform: translate(-100%, 0);
+        }
+    }
+    /*{
+        animation: scrolling-text 1s linear infinite;
+    }*/
+    .stc {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-shrink: 0;
+        overflow-y: hidden;
+    }
+    .st {
+        margin: 2%;
+        border: solid red 1px;
+        text-align: center;
+    }
+    .one {
+        flex: 1;
+        animation: flex-one 3s infinite linear;
+        width: 100px;
+        text-align: center;
+    }
+    .two {
+        flex: 0;
+        animation: flex-two 3s infinite linear;
+        width: 100px;
+        text-align: center;
+    }
+    @if (flex = 0) {
+        display: none;
+    }
+
+    @keyframes flex-one {
+        from {
+            flex: 1;
+        }
+        to {
+            flex: 0;
+        }
+    }
+    @keyframes flex-two {
+        from {
+            flex: 0;
+        }
+        to {
+            flex: 1;
+        }
+    }
+    @keyframes scrolling-text {
+        from {
+            transform: translateX(00%);
+        }
+        to {
+            transform: translateX(-100%);
+        }
+    }
+
     :root {
-        --cr-color: rgba(255, 0, 0, 1);
-        --pr-color: rgba(255, 0, 0, 1);
-        --ft-color: rgba(255, 0, 0, 1);
-        --st-color: rgba(255, 0, 0, 1);
+        --cr-color: rgba(255, 0, 0, 0.3);
+        --pr-color: rgba(255, 0, 0, 0.3);
+        --ft-color: rgba(255, 0, 0, 0.3);
+        --st-color: rgba(255, 0, 0, 0.3);
     }
     * {
         accent-color: purple;
